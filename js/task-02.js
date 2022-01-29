4b215794d811917d74bd6bd0d153934490fdbb8f
Leevorum <di4111ua@gmail.com>
2022-01-29 21:06:52 +0200

    completed second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// Выбираем по ИД список
+const ulEl = document.querySelector('#ingredients')
+
+// Создаем пустой массив для Li
+const elements = [];
+// Проходим циклом по массиву ingredients
+for (let i = 0; i < ingredients.length; i += 1) {
+  // На каждой итерации создаем элемент Ли и добавляем ему ЦСС класс и значение из массива
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredients[i];
+  liEl.classList.add('item');
+// Записываем каждый созданый Ли в массив элементов
+  elements.push(liEl);
+ 
+}
+// Добавляем к нашему списку ЛИ через спред
+ulEl.append(...elements);
