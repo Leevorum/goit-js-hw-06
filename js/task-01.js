88af3b68c59a2c247e98fb31e630edced80dad60
Leevorum <di4111ua@gmail.com>
2022-01-30 00:31:21 +0200

    third task completed

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,13 @@
 // Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
 //Считаем количество категорий
-const elemCategory = document.querySelectorAll('.item');
-console.log('Number of categories:', elemCategory.length);
+const elemCategory = document.querySelectorAll(".item");
+console.log("Number of categories:", elemCategory.length);
 // Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
 // Цикл для прохода массива elemCategory
 
 elemCategory.forEach(element => {
-    // Вывод текста категории
-    console.log('Category:', element.firstElementChild.textContent);
-    // Вывод Количества всех чилдренов
-    console.log('Elements:' , element.lastElementChild.children.length);
+  // Вывод текста категории
+  console.log("Category:", element.firstElementChild.textContent);
+  // Вывод Количества всех чилдренов
+  console.log("Elements:", element.lastElementChild.children.length);
 });
-
--- js/task-02.js
+++ js/task-02.js
@@ -1,25 +1,17 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 // Выбираем по ИД список
-const ulEl = document.querySelector('#ingredients')
+const ulEl = document.querySelector("#ingredients");
 
 // Создаем пустой массив для Li
 const elements = [];
 // Проходим циклом по массиву ingredients
 for (let i = 0; i < ingredients.length; i += 1) {
   // На каждой итерации создаем элемент Ли и добавляем ему ЦСС класс и значение из массива
-  const liEl = document.createElement('li');
+  const liEl = document.createElement("li");
   liEl.textContent = ingredients[i];
-  liEl.classList.add('item');
-// Записываем каждый созданый Ли в массив элементов
+  liEl.classList.add("item");
+  // Записываем каждый созданый Ли в массив элементов
   elements.push(liEl);
- 
 }
 // Добавляем к нашему списку ЛИ через спред
 ulEl.append(...elements);
