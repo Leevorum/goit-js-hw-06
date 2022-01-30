a98ae1eafa6931acc749df198395761b97cfba02
Leevorum <di4111ua@gmail.com>
2022-01-30 23:07:43 +0200

    second task completed

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+// Инпут и спан
+const inputRange = document.querySelector("#font-size-control");
+const spanEl = document.querySelector("#text");
+// Слушатель на инпут
+inputRange.addEventListener("input", event => {
+  // Добавляем инлайн стиль к спан, значение текущее значение range input
+  spanEl.style.fontSize = `${event.currentTarget.value}px`;
+});
