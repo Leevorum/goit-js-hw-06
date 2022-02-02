16801a95b38b6b6f9b2fbb3bd42d95ce4466c47d
Leevorum <di4111ua@gmail.com>
2022-02-02 23:13:55 +0200

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// Получаем ссылки на нужные элементы
+const body = document.querySelector("body");
+const spanColor = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+// Слушатель на кнопку
+changeColorButton.addEventListener("click", () => {
+  // результат функции в переменную
+  let randomColor = getRandomHexColor();
+  // Добавляем цвет в боди
+  body.style.backgroundColor = `${randomColor}`;
+  // Добавляем текстовое значение в спан
+  spanColor.textContent = `${randomColor}`;
+});
